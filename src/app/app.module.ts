d5f1fe9d7f3af2edb34bc1d181d721d49dbe528f
menta <jimementa@gmail.com>
2022-02-19 11:29:29 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from'@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+import { FiltroPipe } from './pokemon/pipe/filtro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    FiltroPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  exports:[
+    PokemonListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//import { FiltroPipe } from './pipe/filtro.pipe';
+
+
+
+@NgModule({
+  declarations: [
+   // FiltroPipe
+  ],
+  imports: [
+    CommonModule
+  ]
+
+})
+export class PokemonModule { }
